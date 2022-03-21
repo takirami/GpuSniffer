040a262beb7a328d86611a41ac9a012c7888e1b8
takirami <jonas.e90@gmail.com>
2022-03-21 23:33:59 +0200

    Fetch 3070 and 3070 TI as well

--- fetchData.js
+++ fetchData.js
@@ -5,7 +5,7 @@ import datatronic from './components/datatronic.js'
 import jimms from './components/jimms.js'
 import proshop from './components/proshop.js'
 import verkkis from './components/verkkis.js'
-import {DATATRONIC_3060, JIMMS_3060, JIMMS_3060_TI, PROSHOP_3060, VERKKIS_3060_AND_TI} from './links.js'
+import {DATATRONIC_3060, DATATRONIC_3060_TI, DATATRONIC_3070, DATATRONIC_3070_TI, JIMMS_3060, JIMMS_3060_TI, JIMMS_3070, JIMMS_3070_TI, PROSHOP_3060_3070_TI, VERKKIS_3060_AND_TI, VERKKIS_3070_AND_TI} from './links.js'
 
 export const fetchData = async () => {
   const loader = 18
@@ -27,19 +27,25 @@ export const fetchData = async () => {
   const storeData = []
   const jimms1 = await jimms(JIMMS_3060)
   const jimms2 = await jimms(JIMMS_3060_TI)
-  storeData.push({name: 'Jimms', data: jimms1.concat(jimms2)})
+  const jimms3 = await jimms(JIMMS_3070)
+  const jimms4 = await jimms(JIMMS_3070_TI)
+  storeData.push({name: 'Jimms', data: jimms1.concat(jimms2, jimms3, jimms4)})
 
   stopStart('Verkkis')
-  const verkkisResults = await verkkis(VERKKIS_3060_AND_TI)
-  storeData.push({name: 'Verkkis', data: verkkisResults})
+  const verkkis1 = await verkkis(VERKKIS_3060_AND_TI)
+  const verkkis2 = await verkkis(VERKKIS_3070_AND_TI)
+  storeData.push({name: 'Verkkis', data: verkkis1.concat(verkkis2)})
 
   stopStart('Proshop')
-  const proshopResults = await proshop(PROSHOP_3060)
+  const proshopResults = await proshop(PROSHOP_3060_3070_TI)
   storeData.push({name: 'Proshop', data: proshopResults})
 
   stopStart('Datatronic')
-  const datatronicResults = await datatronic(DATATRONIC_3060)
-  storeData.push({name: 'Datatronic', data: datatronicResults})
+  const datatronic1 = await datatronic(DATATRONIC_3060)
+  const datatronic2 = await datatronic(DATATRONIC_3060_TI)
+  const datatronic3 = await datatronic(DATATRONIC_3070)
+  const datatronic4 = await datatronic(DATATRONIC_3070_TI)
+  storeData.push({name: 'Datatronic', data: datatronic1.concat(datatronic2, datatronic3, datatronic4)})
   spinner.stop()
   process.stdout.write('\n')
   console.log('-------------------------')
--- links.js
+++ links.js
@@ -1,6 +1,12 @@
 
 export const VERKKIS_3060_AND_TI = 'https://www.verkkokauppa.com/fi/catalog/12149c/NVIDIA-GeForce-RTX-3060?sort=price%3Aasc&filter=ItemAdditionalInformationGraphicsProcessor%3AGeForce+RTX+3060&filter=AvailableImmediatelyAllChannels%3Aimmediately_shippable&filter=AvailableImmediatelyAllChannels%3Ajs&filter=ItemAdditionalInformationGraphicsProcessor%3AGeForce+RTX+3060+Ti'
-export const PROSHOP_3060 = 'https://www.proshop.fi/Naeytoenohjaimet?inv=1&o=1028&pre=0&f~grafikkort_videoudganggrafikprocessorleverandor=nvidia-geforce-rtx-3060~nvidia-geforce-rtx-3060-ti'
+export const VERKKIS_3070_AND_TI = 'https://www.verkkokauppa.com/fi/catalog/12070c/NVIDIA-GeForce-RTX-3070?filter=AvailableImmediatelyAllChannels%3Aimmediately_shippable'
+export const PROSHOP_3060_3070_TI = 'https://www.proshop.fi/Naeytoenohjaimet?f~grafikkort_videoudganggrafikprocessorleverandor=nvidia-geforce-rtx-3060~nvidia-geforce-rtx-3060-ti~nvidia-geforce-rtx-3070~nvidia-geforce-rtx-3070-ti&pre=0&inv=1'
 export const DATATRONIC_3060 = 'https://datatronic.fi/naytonohjaimet/s-2/grafiikkaprosessori-geforce_rtx_3060_ti+geforce_rtx_3060/l%C3%A4hetett%C3%A4viss%C3%A4_heti_myym%C3%A4l%C3%A4ss%C3%A4-kylla'
+export const DATATRONIC_3060_TI = 'https://datatronic.fi/naytonohjaimet/s-2/grafiikkaprosessori-geforce_rtx_3060_ti/l%C3%A4hetett%C3%A4viss%C3%A4_heti_myym%C3%A4l%C3%A4ss%C3%A4-kylla'
+export const DATATRONIC_3070 = 'https://datatronic.fi/naytonohjaimet/s-2/grafiikkaprosessori-geforce_rtx_3070/l%C3%A4hetett%C3%A4viss%C3%A4_heti_myym%C3%A4l%C3%A4ss%C3%A4-kylla'
+export const DATATRONIC_3070_TI = 'https://datatronic.fi/naytonohjaimet/s-2/grafiikkaprosessori-geforce_rtx_3070_ti/l%C3%A4hetett%C3%A4viss%C3%A4_heti_myym%C3%A4l%C3%A4ss%C3%A4-kylla'
 export const JIMMS_3060 = 'https://www.jimms.fi/fi/Product/List/000-1TL/komponentit--naytonohjaimet--geforce-rtx-pelaamiseen--rtx-3060'
 export const JIMMS_3060_TI = 'https://www.jimms.fi/fi/Product/List/000-1T3/komponentit--naytonohjaimet--geforce-rtx-pelaamiseen--rtx-3060-ti'
+export const JIMMS_3070 = 'https://www.jimms.fi/fi/Product/List/000-1SY/komponentit--naytonohjaimet--geforce-rtx-pelaamiseen--rtx-3070'
+export const JIMMS_3070_TI ='https://www.jimms.fi/fi/Product/List/000-1U4/komponentit--naytonohjaimet--geforce-rtx-pelaamiseen--rtx-3070-ti'
